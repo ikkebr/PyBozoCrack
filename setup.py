65949c91468d24d31f7a19edfe542d695081e1ee
Henrique Pereira <ikkibr@gmail.com>
2014-05-08 15:56:29 -0300

    Fixed history

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='pybozocrack',
-    version='1.2.2.1',
+    version='1.2.2.2',
     description='PyBozoCrack is a depressingly effective MD5 password hash cracker with almost zero CPU/GPU load.',
     long_description=readme + '\n\n' + history,
     author='Henrique Pereira',
