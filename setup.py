eb7e708478e4b33e1ea3eb3c64aeda3b9dd1d414
Henrique Pereira <ikkibr@gmail.com>
2021-01-16 15:44:30 -0700

    Skipped PyBozoCrack 2.0 because we only support Python 3 now

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='pybozocrack',
-    version='2.0',
+    version='3.0',
     description='PyBozoCrack is a depressingly effective MD5 password hash cracker with almost zero CPU/GPU load.',
     long_description=readme + '\n\n' + history,
     author='Henrique Pereira',
